65feb12b648b73163e5e248117612bab4a2e6085
александр <xplayeradv@gmail.com>
2020-08-27 14:03:58 +0400

    Убрал в папку

--- Блок1/9_1.sql
+++ Блок1/9_1.sql
@@ -7,3 +7,29 @@ PRINT 'Age: ' + CONVERT(CHAR, @age);
 SET @name='Tom';
 SET @age = 18;
 SELECT @name, @age;
+
+DECLARE @maxPrice MONEY, 
+    @minPrice MONEY, 
+    @dif MONEY, 
+    @count INT
+ 
+SET @count = (SELECT SUM(ProductCount) FROM Orders);
+ 
+SELECT @minPrice=MIN(Price), @maxPrice = MAX(Price) FROM Products
+ 
+SET @dif = @maxPrice - @minPrice;
+ 
+PRINT 'Всего продано: ' + STR(@count, 5) + ' товарa(ов)';
+PRINT 'Разница между максимальной и минимальной ценой: ' + STR(@dif)
+
+DECLARE @sum MONEY, @id INT, @prodid INT;
+SET @id=2;
+ 
+SELECT @sum = SUM(Orders.Price*Orders.ProductCount), 
+     @name=Products.ProductName, @prodid = Products.Id
+FROM Orders
+INNER JOIN Products ON ProductId = Products.Id
+GROUP BY Products.ProductName, Products.Id
+HAVING Products.Id=@id
+ 
+PRINT 'Товар ' + @name + ' продан на сумму ' + STR(@sum)
--- Блок1/9_3.sql
+++ Блок1/9_3.sql
@@ -18,16 +18,3 @@ WHILE @period > 0
  
 SELECT * FROM #Accounts
 
-DECLARE @number INT
-SET @number = 1
- 
-WHILE @number < 10
-    BEGIN
-        PRINT CONVERT(NVARCHAR, @number)
-        SET @number = @number + 1
-        IF @number = 7
-            BREAK;
-        IF @number = 4
-            CONTINUE;
-        PRINT 'Конец итерации'
-    END;
